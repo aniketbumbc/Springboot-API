b1bfd6fc817e96bad0fd38c204a6b36cba1eccf4
aniket <aniket.umbc@gmail.com>
2023-09-22 07:08:12 +0530

    Path variable and Query variable added

--- src/main/java/com/springboot/api/app/StudentController.java
+++ src/main/java/com/springboot/api/app/StudentController.java
@@ -1,6 +1,11 @@
 package com.springboot.api.app;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +17,34 @@ public class StudentController {
 	public Student getStudent() {
 		return new Student("Aniket", "Buunny");
 	}
+
+	@GetMapping("/getStudents")
+	public List<Student> getStudents() {
+
+		List<Student> students = new ArrayList<>();
+
+		students.add(new Student("Miket", "Yuum"));
+		students.add(new Student("Masket", "Yuum"));
+		students.add(new Student("Yaho", "Yuum"));
+		students.add(new Student("Gorge", "Yuum"));
+
+		return students;
+	}
+	
+	
+	// rest API using path variable
+	@GetMapping("/singleStudent/{firstName}/{lastName}")
+	public Student studentPathVariable(@PathVariable("firstName") String firstName,
+			@PathVariable("lastName") String lastName) {
+
+		return new Student(firstName, lastName);
+	}
+	
+	
+	// rest API using queryParams
+		@GetMapping("/singleStudent")
+		public Student studentQueryParams(@RequestParam(name = "firstName") String firstName, @RequestParam   (name = "lastname") String lastName) {
+				return new Student(firstName, lastName);
+		}
 
 }
